192ae32f54d1f4c785d4ad80806dca8f218ccbfb
flparedes <franciscoluis.paredes@gmail.com>
2015-07-06 14:00:09 +0200

    Layout y controller
    Añadido el Layout y el controlador para el proyecto.

---
+++ controllers/quiz_controllers.js
@@ -0,0 +1,10 @@
+// Controlador para el GET /quizes/question
+exports.question = function(req, res) {
+	res.render('quizes/question', {pregunta: '¿Cuál es la capital de Italia?'});
+};
+
+// Controlador para el GET /quizes/answer
+exports.answer = function(req, res) {
+	var resultado = req.query.respuesta === 'Roma' ? 'Correcto' : 'Incorrecto';
+	res.render('quizes/answer', {respuesta: resultado});
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// Controlador de quizes
+var quizController = require('../controllers/quiz_controllers');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
 });
+
+// GET para /quizes/question
+router.get('/quizes/question', quizController.question);
+
+// GET para /quizes/answer
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
