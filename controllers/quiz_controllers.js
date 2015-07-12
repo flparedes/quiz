76e09e9a0f2058781574214ddb3c412349f9bf6e
flparedes <franciscoluis.paredes@gmail.com>
2015-07-12 10:41:38 +0200

    Búsqueda de preguntas
    Se añade la búsqueda de preguntas en la ventana de lista de preguntas

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -18,9 +18,36 @@ exports.load = function(req, res, next, quizId) {
 
 // Controlador para el GET /quizes
 exports.index = function(req, res) {
+	var search = req.query.search;
+
+	if (search) {
+		buscarConSearch(req, res, search);
+	} else {
+		buscarTodos(req, res);
+	}
+};
+
+// Función para la búsqueda sin parámetros en search
+function buscarTodos(req, res) {
 	// Se realiza una búsqueda en Base de Datos.
 	models.Quiz.findAll().then(function (quizes) {
 		//res.render('quizes/question', {pregunta: '¿Cuál es la capital de Italia?'});
+		res.render('quizes/index', {preguntas: quizes});
+	});
+}
+
+// Función para la búsqueda con parámetros en search
+function buscarConSearch(req, res, search) {
+	// Se reemplazan los espacios en blanco por %
+	search = search.replace(/ /g, "%");
+
+	models.Quiz.findAll({
+		where: {
+			pregunta: {
+				$like: '%' + search + '%'
+			}
+		}
+	}).then(function (quizes) {
 		res.render('quizes/index', {preguntas: quizes});
 	});
 };
